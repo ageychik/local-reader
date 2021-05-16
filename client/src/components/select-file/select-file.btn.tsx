3eb1572ca4af925e48686a1a303a652abdacab29
Your Name <you@example.com>
2021-05-16 19:15:46 +0300

    added mixin for font-size

--- client/src/components/select-file/select-file.btn.tsx
+++ client/src/components/select-file/select-file.btn.tsx
@@ -1,36 +1,42 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFile, faFolder, faLink} from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import React, { useState } from "react";
 
 const initialState = Object.freeze({
     selectType: [
-        {icon: faFolder},
-        {icon: faFile},
-        {icon: faLink},
+        {icon: faFile, value: 'Select Files', active: true},
+        {icon: faFolder, value: 'Select Folder', active: false},
+        {icon: faLink, value: 'Select Url', active: false},
     ]
 })
 
-export default class SelectFileBtn extends React.Component {
-    state = initialState;
+const SelectFileBtn: React.FC = () => {
+    // const [ a, b ] = useState<string>('');
 
-    render() {
-        let { selectType } = this.state;
-        return (
-            <section className="select-file">
-                <label htmlFor="select-files" className="select-file-btn">
+    const selectType = [
+        {icon: faFile, value: 'Select Files', active: true},
+        // {icon: faFolder, value: 'Select Folder', active: false},
+        // {icon: faLink, value: 'Select Url', active: false},
+    ]
+
+    return (
+        <section className="select-file">
+            <label htmlFor="select-files" className="select-file-btn">
 
                         <span className="select-file-input">
                             <span>Select Files</span>
                             <input id="select-files" type="file" hidden/>
                         </span>
 
-                    <ul className="select-file-type">
-                        {selectType.map((el, i) => {
-                            return (<li key={i}><FontAwesomeIcon icon={ el.icon }/></li>)
-                        })}
-                    </ul>
-                </label>
-            </section>
-        )
-    }
+                <ul className="select-file-type">
+                    {selectType.map((el, i) => {
+                        return (
+                            <li className={ el.active ? 'active' : '' } key={i}><FontAwesomeIcon icon={ el.icon }/></li>)
+                    })}
+                </ul>
+            </label>
+        </section>
+    )
 }
+
+export default SelectFileBtn;
