00c666bd6197c6d03fff82ca0ac2bb968f9ac52e
Your Name <you@example.com>
2021-05-16 13:47:02 +0300

    added react and scss to project

---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component{
+    render () {
+        return (
+            <header className="App-header">
+                <h1>Hello world</h1>
+            </header>
+        )
+    }
+}
